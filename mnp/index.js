829b5ee42185c0d220cf0a7ee1ef6503c58ad930
Anton <anton@adc.sh>
2019-12-03 08:12:58 +0300

    mnp questions

--- mnp/index.js
+++ mnp/index.js
@@ -1,19 +1,6 @@
 export default {
+  mnpQuestions: ['wiki', 'license', 'homepage'],
   questions: {
-    'wiki': {
-      text: 'Init Github Wiki',
-      confirm: true,
-      async afterQuestions({ confirm, spawn }, answer, { name, org }) {
-        if (answer) {
-          const a = await confirm(`Please go to https://github.com/${org}/${name}/wiki/_new
-to create the first page and press enter when done.`)
-          if (!a) return
-          const m = `git@github.com:${org}/${name}.wiki.git`
-          await spawn('git', ['submodule', 'add', m, 'wiki'])
-          return m
-        }
-      },
-    },
     binary: {
       confirm: true,
       text: 'With binary',
