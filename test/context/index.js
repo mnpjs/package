878669423fee92a853a59cdb48482fadf1fb5725
Anton <anton@adc.sh>
2020-01-28 23:42:10 +0300

    add context

--- test/context/index.js
+++ test/context/index.js
@@ -42,3 +42,11 @@ if (process.env.ALAMODE_ENV == 'test-build') {
   console.log('Testing compile bin...')
   BIN = 'compile/bin/mnp'
 }
+
+/** @typedef {Object<string, Test & TestSuite4>} TestSuite */
+/** @typedef {Object<string, Test & TestSuite3>} TestSuite4 */
+/** @typedef {Object<string, Test & TestSuite2>} TestSuite3 */
+/** @typedef {Object<string, Test & TestSuite1>} TestSuite2 */
+/** @typedef {Object<string, Test & TestSuite0>} TestSuite1 */
+/** @typedef {Object<string, Test>} TestSuite0 */
+/** @typedef {(c: Context)} Test */
