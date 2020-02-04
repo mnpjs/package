b42f5f258f70a12861d234e03ed0d46f078db079
Anton <anton@sobesednik.media>
2020-02-04 05:22:46 +0300

    up alamode + fix build bin

--- mnp/index.js
+++ mnp/index.js
@@ -1,4 +1,5 @@
 import preUpdate from './pre-update'
+import compile from './compile'
 
 export default {
   mnpQuestions: ['wiki', 'license', 'homepage', 'keywords'],
@@ -61,57 +62,7 @@ export default {
         }, { file: 'test/context/index.js' })
       },
     },
-    compile: {
-      text: 'Build or compile',
-      getDefault() { return 'compile' },
-      async afterQuestions({ rm, removeFile, packageJson, updatePackageJson, updateFiles, json, saveJson, renameFile }, answer, { binary }) {
-        const compile = answer == 'compile'
-        const build = answer == 'build'
-        const { scripts } = packageJson
-        delete scripts.d2 // manually run on the build
-        // this should be reconsiled with @methodType regex in Typal on source
-        const alamoderc = json('.alamoderc.json')
-        if (compile) {
-          await rm('build')
-          await rm('stdlib')
-          removeFile('src/index-build.js')
-          removeFile('src/stdlib.js')
-          delete scripts['test-build']
-          delete scripts['stdlib']
-          delete scripts['b']
-          delete alamoderc.env['test-build']
-          delete alamoderc.env['build'] // remove stdlib
-          packageJson.files = packageJson.files.filter((a) => {
-            return !['build', 'stdlib'].includes(a)
-          })
-          await updateFiles({
-            re: /if (process.env.ALAMODE_ENV == 'test-build') {[\s\S]+?} else /,
-            replacement: '',
-          }, { file: 'test/context/index.js' })
-          // import types from compile/index.js
-          // await updateFiles({
-          //   re: /import\('\.\.\/types'\)/g,
-          //   replacement: 'import(\'..\')',
-          // }, { file: 'src/index.js' })
-        } else if (build) {
-          await rm('src/index.js')
-          await rm('build/index.js')
-          renameFile('src/index-build.js', 'src/index.js')
-          renameFile('build/index-build.js', 'build/index.js')
-          removeCompile(alamoderc, scripts, packageJson, binary)
-          await rm('compile')
-          removeFile('src/depack.js')
-          removeFile('build/depack.js')
-          await updateFiles({
-            re: / else if (process.env.ALAMODE_ENV == 'test-compile') {[\s\S]+?}/,
-            replacement: '',
-          }, { file: 'test/context/index.js' })
-        }
-        packageJson.scripts = scripts
-        updatePackageJson(packageJson)
-        saveJson('.alamoderc.json', alamoderc)
-      },
-    },
+    compile,
   },
   preUpdate,
   async afterInit({ name }, { renameFile, initManager }) {
@@ -126,33 +77,4 @@ export default {
     const i = 'initialise package'
     await git('tag', '-a', 'v0.0.0-pre', '-m', process.platform == 'win32' ? `"${i}"` : i)
   },
-}
-
-/**
- * 1. Since building, move dev-deps to deps.
- */
-const removeCompile = async (alamoderc, scripts, packageJson, bin) => {
-  const { devDependencies, dependencies = {} } = packageJson
-  const {
-    argufy, indicatrix, usually, ...restDevDependencies
-  } = devDependencies
-  if (bin) Object.assign(dependencies, { argufy, indicatrix, usually })
-
-  if (!bin) alamoderc.env.build.import.alamodeModules = ['erte']
-  if (!bin) alamoderc.env.build.import.stdlib.packages = ['erte']
-  delete alamoderc.env['test-compile']
-  delete alamoderc.import
-  delete scripts.template
-  scripts.d1 = 'typal src -u -t types/index.xml'
-  delete scripts['test-compile']
-  delete scripts['compile']
-  delete scripts['lib']
-  packageJson.main = 'build/index.js'
-  packageJson.files = packageJson.files.filter((a) => {
-    return a != 'compile'
-  })
-  Object.assign(packageJson, {
-    devDependencies: restDevDependencies,
-  })
-  if (Object.keys(dependencies).length) Object.assign(packageJson, { dependencies })
 }
